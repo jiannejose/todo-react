508f6a6b3d2dc354289bc618de6d2bc900e9760c
Anne Jizrelle Jose <annejizrelle@gmail.com>
2018-04-30 19:48:03 +0800

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>To-Do List</h1>
+
+        <button>+ Add Task</button>
+      </div>
+      
+    )
+  }
+}
+
+export default App;
